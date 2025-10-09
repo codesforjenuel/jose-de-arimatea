a1ec684edc6926408c15dc73c4073b0ef5ae2ee8
Jesus Perea <g-sus-15@hotmail.com>
2025-10-09 00:34:07 -0700

    added the banner container, img, text and button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,11 @@
 export default function Header() {
     return (
-        <div></div>
+        <div className="header">
+<img className="headerImg" src="https://res.cloudinary.com/dkaeetuud/image/upload/v1759993239/jose%20de%20arimatea/jose_de_arimatea_logo_egvwsx.png" alt="" />
+<div className="headerContainer">
+<p>Cada Acto de Bondad es una semilla plantada para la Eternidad</p>
+        <button>Donacion</button>
+        </div>
+        </div>
     )
 }
